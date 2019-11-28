f4f84d5c0ee4b89cdfadd374240f1d6d22ac5485
Панов Антон <A.Panov@expobank.ru>
2019-11-28 17:43:31 +0300

    update

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,10 +10,11 @@
 
       <template v-slot:before>
         <q-tabs
-          v-model="sCurrentTextBoxTab"
+          v-model="sSelectedTextBox"
           vertical
           class=""
         >
+          <q-btn color="primary" label="Add" @click="fnAddTextBox" unelevated class="col full-width"/>
           <q-tab 
             v-for="(oTextBox, sTextBoxID) in oTextBoxes"
             :name="sTextBoxID" 
@@ -24,8 +25,7 @@
 
       <template v-slot:after>
         <q-tab-panels
-          v-model="sCurrentTextBoxTab"
-          animated
+          v-model="sSelectedTextBox"
           transition-prev="jump-up"
           transition-next="jump-up"
           class="col"
@@ -37,7 +37,19 @@
           >
 
               <div class="col-8 column">
-                <div class="col-auto text-h4 q-mb-md">{{ oTextBox.sName }}</div>
+                <!--div class="col-auto text-h6 q-mb-sm">{{ oTextBox.sName }}</div-->
+                <div class="col-auto q-mb-sm row">
+                  <q-input
+                    ref="textbox_name"
+                    outlined 
+                    dense
+                    square
+                    v-model="oTextBox.sName" 
+                    label="Name" 
+                    class="col"
+                  />
+                  <q-btn color="negative" label="Remove" @click="fnRemoveTextBox" square unelevated class="col-auto"/>
+                </div>
                 <codemirror
                   ref="cm_editor"
                   :value="oTextBox.sTextArea" 
@@ -49,9 +61,47 @@
                   style="width:100%"
                 >
                 </codemirror>
+                <div class="col-6">
+                  <q-tabs
+                    v-model="oTextBox.sSelectedResultTab"
+                    dense
+                    align="left"
+                    active-color="primary"
+                    indicator-color="primary"
+                    narrow-indicator
+                  >
+                    <q-tab 
+                      :name="sResultID" 
+                      :label="oResultItem.sName" 
+                      v-for="(oResultItem, sResultID) in oTextBox.oResults"
+                    />
+                    <q-btn 
+                      color="primary" 
+                      icon="close" 
+                      @click="fnRemoveResultTab(oTextBox, sResultID)" 
+                      flat 
+                      unelevated 
+                      class="col-auto q-ml-auto"
+                      v-if="oTextBox.sSelectedResultTab"
+                    />
+                  </q-tabs>
+
+                  <q-separator />
+
+                  <q-tab-panels 
+                    v-model="oTextBox.sSelectedResultTab" 
+                  >
+                    <q-tab-panel 
+                      :name="sResultID" 
+                      v-for="(oResultItem, sResultID) in oTextBox.oResults"
+                    >
+                      {{ oResultItem.sName }}
+                    </q-tab-panel>
+                  </q-tab-panels>
+                </div>             
               </div>
-              <div class="col-4 column q-px-md">
-                <div class="col-auto text-h5 q-mb-md">Filters</div>
+              <div class="col-4 column q-pl-md">
+                <div class="col-auto text-h6 q-mb-sm">Filters</div>
                 <div class="col-5 column">
                   <div class="row col-auto">
                     <q-input 
@@ -97,7 +147,7 @@
                   <q-scroll-area
                     :thumb-style="oScollbarThumbStyle"
                     :bar-style="oScollbarBarStyle"
-                    style=""
+                    style="border: 1px solid #eee"
                     class="col"
                   >
                     <q-list bordered separator>
@@ -107,12 +157,14 @@
                         clickable 
                         v-ripple
                         dense
+                        :active="sSelectedFilter==sFilterID"
+                        @click="sSelectedFilter = sFilterID"
                       >
                         <q-item-section side top>
                           <q-checkbox v-model="oFilter.bSelected" />
                         </q-item-section>
                         <q-item-section>
-                          <q-item-label>{{ oFilter.sName }}</q-item-label>
+                          <q-item-label>{{ oFilter.sName ? oFilter.sName : 'Undefined' }}</q-item-label>
                           <q-item-label caption lines="2">
                             <small v-if="oFilter.sType=='regexp'">/{{ oFilter.sRegExp }}/{{ oFilter.sFlags }}</small>
                             <small v-if="oFilter.sType=='text'">{{ oFilter.sSubString }}</small>
@@ -127,8 +179,82 @@
                     </q-list>
                   </q-scroll-area>
                 </div>
-                <div class="col-6">
-                  123
+                <div class="col-6 column q-pt-md" v-if="sSelectedFilter!=''">
+                  <!--div class="col-auto text-h6 q-mb-sm">{{ oFilters[sSelectedFilter].sName }}</div-->
+                  <q-input
+                    ref="filter_name"
+                    outlined 
+                    square
+                    dense
+                    v-model="oFilters[sSelectedFilter].sName" 
+                    label="Name" 
+                    class="col-auto q-mb-sm"
+                  />
+
+                  <q-select 
+                    class="col-auto q-mb-sm" 
+                    filled 
+                    square
+                    dense
+                    v-model="oFilters[sSelectedFilter].sType" 
+                    :options="aFiltersTypes" 
+                    label="Type" 
+                    @input="(v) => fnChangeFilterType(v, sSelectedFilter)" 
+                  />
+
+                  <div class="col-auto column" v-show="oFilters[sSelectedFilter].sType=='regexp'">
+                    <q-input 
+                      square
+                      dense
+                      outlined 
+                      v-model="oFilters[sSelectedFilter].sRegExp" 
+                      label="Regular expression" 
+                      class="col-auto q-mb-sm"
+                    />
+                    <q-input 
+                      ref="filter_flags"
+                      outlined 
+                      square
+                      dense
+                      v-model="oFilters[sSelectedFilter].sFlags" 
+                      label="Flags" 
+                      class="col-auto q-mb-sm"
+                      :rules="[
+                        v => /^[gmi]+$/.test(v) || 'Wrong flag',
+                        v => !v.split('').some((v,i,a) => a.lastIndexOf(v)!=i ) || 'Duplicate flag'
+                      ]"
+                    />
+                    <q-input
+                      v-model="oFilters[sSelectedFilter].sReplacement"
+                      square
+                      dense
+                      outlined
+                      type="textarea"
+                      label="Replacement"
+                    />
+                  </div>
+                  <div class="col-auto column" v-show="oFilters[sSelectedFilter].sType=='text'">
+                    <q-input outlined v-model="oFilters[sSelectedFilter].sSubString" label="String" class="col-auto q-mb-sm"/>
+                    <q-input
+                      v-model="oFilters[sSelectedFilter].sReplacement"
+                      square
+                      dense
+                      outlined
+                      type="textarea"
+                      label="Replacement"
+                    />
+                  </div>
+                  <div class="col column" v-show="oFilters[sSelectedFilter].sType=='function'">
+                    <codemirror
+                      ref="filter_cm_editor"
+                      :value="oFilters[sSelectedFilter].sFunction" 
+                      :options="oFilterCmOptions"
+                      @input="(sNewText) => { fnOnFilterCmCodeChange(sNewText, sSelectedFilter) }"
+                      class="flex col"
+                      style="width:100%; height:100%"
+                    >
+                    </codemirror>
+                  </div>
                 </div>
               </div>
 
@@ -158,15 +284,29 @@
 .dropdown-without-arrow .q-btn-dropdown__arrow {
   display: none;
 }
+.q-tab__label {
+  word-break: break-all;
+  word-wrap: break-word;
+  white-space: normal;
+}
 </style>
 
 <script>
 
 // require component
 import { codemirror } from 'vue-codemirror'
+import Vue from 'vue'
 
 // require styles
 import 'codemirror/lib/codemirror.css'
+import 'codemirror/addon/edit/matchbrackets.js'
+import 'codemirror/addon/comment/continuecomment.js'
+import 'codemirror/addon/comment/comment.js'
+import 'codemirror/mode/javascript/javascript.js'
+
+import cuid from 'cuid';
+
+import fs from 'fs';
 
 const utils = require('~/lib/utils.js');
 
@@ -174,11 +314,16 @@ const FILTER_REGEXP = "regexp";
 const FILTER_TEXT = "text";
 const FILTER_FUNCTION = "function";
 
-const aFiltersTypes = [
-  FILTER_REGEXP,
-  FILTER_TEXT,
-  FILTER_FUNCTION
-];
+const sDataFilePath = './data.json';
+
+import { Notify } from 'quasar'
+
+Notify.setDefaults({
+  position: 'top-right',
+  timeout: 2500,
+  textColor: 'white',
+  actions: [{ icon: 'close', color: 'white' }]
+})
 
 export default {
   name: 'PageIndex',
@@ -190,9 +335,16 @@ export default {
   data()
   {
     return {
+      aFiltersTypes: [
+        FILTER_REGEXP,
+        FILTER_TEXT,
+        FILTER_FUNCTION
+      ],
+
       oTextBoxes: {
         guid1: {
           sName: 'Undefined',
+          sSelectedResultTab: "",
           sTextArea: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.",
           oResults: {
             guid1: {
@@ -207,6 +359,7 @@ export default {
         },
         guid2: {
           sName: 'Undefined 2',
+          sSelectedResultTab: "",
           sTextArea: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.",
           oResults: {
             guid1: {
@@ -244,11 +397,22 @@ export default {
         }
       },
 
-
+      sSelectedFilter: "",
+      sSelectedFilterType: "",
+      sSelectedTextBox: '',
 
       oApplicationSettings: {
         sHomePath: '',
         sConfigurationPath: ''
+      },
+
+      oFilterCmOptions: {
+        mode: 'javascript',
+        lineNumbers: true,
+        lineWrapping: true,
+        matchBrackets: true,
+        continueComments: "Enter",
+        extraKeys: {"Ctrl-Q": "toggleComment"}
       },
 
       oCmOptions: {
@@ -257,8 +421,8 @@ export default {
       },
       sFiltersFilterText: '',
 
-      iTextBoxSplitter: 10,
-      sCurrentTextBoxTab: ''
+      iSaveTimeout: 5000,
+      iTextBoxSplitter: 10
     }
   },
 
@@ -297,29 +461,156 @@ export default {
     },
     fnOnCmCodeChange(sNewText, oTextBox)
     {
-      oTextBox.sTextArea = sNewText
+      oTextBox.sTextArea = sNewText;
+    },
+    fnOnFilterCmCodeChange(sNewText, sSelectedFilter)
+    {
+      this.oFilters[sSelectedFilter].sFunction = sNewText;
     },
     fnFilterFilter(oFilter)
     {
       return ~oFilter.sName.indexOf(this.sFiltersFilterText);
     },
+    fnSelectFilter(sFilterID)
+    {
+      // this.sSelectedFilter = sFilterID;
+      // sSelectedFilterType
+    },
+    fnChangeFilterType(sValue, sSelectedFilter)
+    {
+      var aAttributes = [ "sRegExp", "sFlags", "sSubString", "sFunction" ];
+
+      var oFilter = this.oFilters[sSelectedFilter];
+
+      aAttributes.forEach((v) => Vue.delete(oFilter, v) );
+
+      if (sValue==FILTER_REGEXP) {
+        Vue.set(oFilter, 'sRegExp', '');
+        Vue.set(oFilter, 'sFlags', '');
+      }
+      if (sValue==FILTER_TEXT) {
+        Vue.set(oFilter, 'sSubString', '');
+      }
+      if (sValue==FILTER_FUNCTION) {
+        Vue.set(oFilter, 'sFunction', '');
+      }
+
+      oFilter.sType = sValue;
+    },
     fnAddFilter()
     {
+      var sCUID = cuid();
 
+      Vue.set(this.oFilters, sCUID, {
+        bSelected: false,
+        sName: "",
+        sType: "regexp",
+        sRegExp: "",
+        sFlags: "g",
+        sReplacement: ""
+      });
+
+      this.sSelectedFilter = sCUID;
     },
     fnRemoveFilters()
     {
+      var oThis = this;
+      var aKeys = Object.keys(oThis.oFilters);
 
+      aKeys.forEach((sKey) => { 
+        var oFilter = oThis.oFilters[sKey];
+
+        if (oFilter.bSelected) {
+          Vue.delete(oThis.oFilters, sKey);
+          
+          if (oThis.sSelectedFilter == sKey) {
+            oThis.sSelectedFilter = sKey;
+          }
+        }
+      });
     },
     fnExecuteFilters()
     {
 
+    },
+    fnAddTextBox()
+    {
+      var sCUID = cuid();
+
+      Vue.set(
+        this.oTextBoxes, 
+        sCUID, 
+        {
+          sName: '',
+          sSelectedResultTab: "",
+          sTextArea: "",
+          oResults: {
+          }
+        }
+      );
+
+      this.sSelectedTextBox = sCUID;
+    },
+    fnRemoveTextBox()
+    {
+      Vue.delete(this.oTextBoxes, this.sSelectedTextBox);
+
+      this.sSelectedTextBox = '';
+    },
+    fnRemoveResultTab(oTextBox, sResultID)
+    {
+      Vue.delete(oTextBox.oResults, sResultID);
+
+      oTextBox.sSelectedResultTab = utils.fnGetFirstKey(oTextBox.oResults);
+    },
+    fnNotifyError(sMessage)
+    {
+      this.$q.notify({ color: 'negative', message: sMessage, icon: 'report_problem' });
+    },
+    fnNotifySuccess(sMessage)
+    {
+      this.$q.notify({ message: sMessage, icon: 'thumb_up' });
+    },
+    fnSaveData()
+    {
+      var sData = JSON.stringify(this.$data, null, 4);
+
+      try {
+        fs.writeFileSync(sDataFilePath, sData);
+      } catch (oError) {
+        this.fnNotifyError(oError.toString());
+        return;
+      }
+
+      //this.fnNotifySuccess("Data saved");
+    },
+    fnSaveLoop()
+    {
+      this.fnSaveData();
+
+      setTimeout(this.fnSaveLoop, this.iSaveTimeout);
     }
   },
 
   created()
   {
-    this.sCurrentTextBoxTab = utils.fnGetFirstKey(this.oTextBoxes);
+    if (fs.existsSync(sDataFilePath)) {
+      try {
+        var oThis = this;
+        var oData = JSON.parse(fs.readFileSync(sDataFilePath).toString());
+        var aKeys = Object.keys(oData);
+
+        aKeys.forEach((sKey) => { 
+          Vue.set(oThis.$data, sKey, oData[sKey]);
+        });
+      } catch (oError) {
+        this.fnNotifyError(oError.toString());
+      }
+    }
+
+    this.sSelectedTextBox = utils.fnGetFirstKey(this.oTextBoxes);
+
+    this.fnSaveLoop();
   }
 }
 </script>
